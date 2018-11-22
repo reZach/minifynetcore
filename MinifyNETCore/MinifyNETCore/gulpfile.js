4db7d25238cbf11f27ae33c44ce5e1329d47e0e6
Zachary Wenta <reZach@users.noreply.github.com>
2018-11-21 22:37:39 -0600

    renaming the gulp file so VS picks up the gulp task in task runner explorer

--- MinifyNETCore/MinifyNETCore/gulpfile.js
+++ MinifyNETCore/MinifyNETCore/gulpfile.js
@@ -2,9 +2,13 @@
 var rename = require('gulp-rename');
 var minifyCshtml = require('gulp-cshtml-minify');
 
-gulp.task('minify', function () {
+gulp.task('minify', function (done) {
     gulp.src("Views/**")
         .pipe(minifyCshtml())
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("~/Views/pie"));
+
+    // Needed for gulp 4.x;
+    // https://stackoverflow.com/questions/36897877/gulp-error-the-following-tasks-did-not-complete-did-you-forget-to-signal-async
+    done();
 });
