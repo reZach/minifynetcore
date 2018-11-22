3dcf15897606fb3ebbf1750787b32fb3c6ffd2c5
Zachary Wenta <reZach@users.noreply.github.com>
2018-11-21 22:34:22 -0600

    partially adding minification

---
+++ MinifyNETCore/MinifyNETCore/gulp.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp');
+var rename = require('gulp-rename');
+var minifyCshtml = require('gulp-cshtml-minify');
+
+gulp.task('minify', function () {
+    gulp.src("Views/**")
+        .pipe(minifyCshtml())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("~/Views/pie"));
+});
