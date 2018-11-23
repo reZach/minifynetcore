57cb4d26d16e3776865f5f8c34451916204d251b
Zachary Wenta <reZach@users.noreply.github.com>
2018-11-22 23:50:56 -0600

    Minifying works now; beginning to add base controller

---
+++ MinifyNETCore/MinifyNETCore/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinifyNETCore.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public override ViewResult View()
+        {
+
+        }
+
+        public override ViewResult View(string viewName, object model)
+        {
+            return base.View(model);
+        }
+
+        public override ViewResult View(object model)
+        {
+            return base.View(model);
+        }
+
+        public override ViewResult View(string viewName)
+        {
+            return base.View(viewName);
+        }
+    }
+}
--- MinifyNETCore/MinifyNETCore/gulpfile.js
+++ MinifyNETCore/MinifyNETCore/gulpfile.js
@@ -3,10 +3,10 @@ var rename = require('gulp-rename');
 var minifyCshtml = require('gulp-cshtml-minify');
 
 gulp.task('minify', function (done) {
-    gulp.src("Views/**")
-        .pipe(minifyCshtml())
+    gulp.src("Views/**")        
         .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("~/Views/pie"));
+        .pipe(minifyCshtml())
+        .pipe(gulp.dest("Views"));
 
     // Needed for gulp 4.x;
     // https://stackoverflow.com/questions/36897877/gulp-error-the-following-tasks-did-not-complete-did-you-forget-to-signal-async
