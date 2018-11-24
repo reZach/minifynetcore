545846fb6c834edeb797dc73e15272cda98df6b5
Zachary Wenta <reZach@users.noreply.github.com>
2018-11-23 22:44:48 -0600

    Got to load minified partials correctly!

--- MinifyNETCore/MinifyNETCore/TagHelpers/MPartialTagHelper.cs
+++ MinifyNETCore/MinifyNETCore/TagHelpers/MPartialTagHelper.cs
@@ -18,67 +18,14 @@ namespace MinifyNETCore.TagHelpers
 
         }
 
-        public override void Init(TagHelperContext context)
-        {
-            var allAttributes = context.AllAttributes;
-            var newAttributes = new List<TagHelperAttribute>();
-            string name = string.Empty;
-            object value;
-
-            for (int i = 0; i < allAttributes.Count; i++)
-            {
-                name = allAttributes.ElementAt(i).Name;
-                value = allAttributes.ElementAt(i).Value;
-
-                if (name.Equals("name", StringComparison.OrdinalIgnoreCase)
-                    && IsDevelopment())
-                {
-                    value += ".min";
-                }
-
-                newAttributes.Add(new TagHelperAttribute(name, value, allAttributes[i].ValueStyle));
-            }
-
-            TagHelperContext newContext = new TagHelperContext(context.TagName,
-                new TagHelperAttributeList(newAttributes), context.Items, context.UniqueId);
-            base.Init(newContext);
-        }
-
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            var a = context.AllAttributes;
-            //if (!IsDevelopment())
-            //{
-            //    context.TagName += ".min";
-            //}
-
-            base.Process(context, output);
-        }
-
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            //var existing = context.AllAttributes;
+            // Tack on a .min exension to load the minified partial view
+            if (!IsDevelopment())
+            {
+                Name += ".min";
+            }
 
-            //context.AllAttributes.Append(new TagHelperAttribute("a", "b"));
-            //var updated = new Dictionary<object, object>();
-
-            //string name = string.Empty;
-            //object value;
-            //for (int i = 0; i < existing.Count; i++)
-            //{
-            //    name = existing.ElementAt(i).Name;
-            //    value = existing.ElementAt(i).Value;
-
-            //    if (name.Equals("name", StringComparison.OrdinalIgnoreCase)
-            //        && IsDevelopment())
-            //    {
-            //        value += ".min";
-            //    }
-
-            //    updated.Add(name, value);
-            //}
-            
-            //context.Reinitialize(context.TagName, updated, context.UniqueId);
             return base.ProcessAsync(context, output);
         }
 
